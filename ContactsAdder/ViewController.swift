d4faecdd0109b3d831780fcb6de726383c1e324b
Gabriel Revells <voltage@nat-tvwna-inside-visitornet101-c-24877.princeton.org>
2016-06-25 23:56:08 -0400

    Initial Commit

--- ContactsAdder/ViewController.swift
+++ ContactsAdder/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import Contacts
+
+let FAMILY_NAME = "iD Tech Test"
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let contacts = CNContactStore()
+        
+        let status = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+        
+        switch status {
+        case .authorized:
+            // UM
+            print("Authorized")
+            
+        case .restricted, .denied:
+            print("NOPE")
+            
+        case .notDetermined:
+            print("WHO KNOWS")
+        }
+        
+        contacts.requestAccess(for: .contacts) { (allowed, nil) in
+            
+        }
+        
+        let count = (UIApplication.shared().delegate as! AppDelegate).count
+        
+        if count > 0 {
+            let alert = UIAlertController(title: "Whoops", message: "Failed to add \(count) contacts. Try again?", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func deleteContacts(_ sender: AnyObject) {
+        let pred = CNContact.predicateForContacts(matchingName: "iD Tech")
+        
+        let store = CNContactStore()
+        
+        let contacts = try! store.unifiedContacts(matching: pred, keysToFetch: [CNContactPhoneNumbersKey, CNContactGivenNameKey, CNContactFamilyNameKey])
+        
+        let req = CNSaveRequest()
+        
+        for contact in contacts {
+            if contact.familyName == FAMILY_NAME {
+                
+                req.delete(contact.mutableCopy())
+            }
+        }
+    }
 
 }
 
